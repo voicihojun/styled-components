1835bfcf212c88384c8c72f539aa4722fdef7375
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2021-03-23 15:19:39 -0400

    dialog completion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import Button from "./components/Button";
+import Dialog from "./components/Dialog";
 
 const AppBlock = styled.div`
   width: 512px;
@@ -17,6 +18,21 @@ const ButtonGroup = styled.div`
 `;
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  };
+
+  const onConfirm = () => {
+    console.log("confirm");
+    setDialog(false);
+  };
+
+  const onCancel = () => {
+    console.log("cancel");
+    setDialog(false);
+  };
+
   return (
     <ThemeProvider
       theme={{
@@ -27,48 +43,60 @@ function App() {
         },
       }}
     >
-      <AppBlock>
-        <ButtonGroup>
-          <Button size="large">BUTTON</Button>
-          <Button>BUTTON</Button>
-          <Button size="small">BUTTON</Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="gray" size="large">
-            BUTTON
-          </Button>
-          <Button color="gray">BUTTON</Button>
-          <Button color="gray" size="small">
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="pink" size="large" outline>
-            BUTTON
-          </Button>
-          <Button color="pink" outline>
-            BUTTON
-          </Button>
-          <Button color="pink" size="small" outline>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="blue" size="medium" fullWidth>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="gray" size="medium" fullWidth>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="pink" size="medium" fullWidth>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-      </AppBlock>
+      <>
+        <AppBlock>
+          <ButtonGroup>
+            <Button size="large">BUTTON</Button>
+            <Button>BUTTON</Button>
+            <Button size="small">BUTTON</Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="gray" size="large">
+              BUTTON
+            </Button>
+            <Button color="gray">BUTTON</Button>
+            <Button color="gray" size="small">
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="pink" size="large" outline>
+              BUTTON
+            </Button>
+            <Button color="pink" outline>
+              BUTTON
+            </Button>
+            <Button color="pink" size="small" outline>
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="blue" size="medium" fullWidth>
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="gray" size="medium" fullWidth>
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="pink" size="medium" fullWidth onClick={onClick}>
+              DELETE
+            </Button>
+          </ButtonGroup>
+        </AppBlock>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+          visible={dialog}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 }
