5c8c5bb3d87a77af60e1b5258f208fcdbb9fe5f8
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2021-03-23 13:29:27 -0400

    fisrt component using styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+  background: ${(props) => props.color || "red"};
+
+  ${(props) =>
+    props.huge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Circle color="blue" huge />;
 }
 
 export default App;
